83b9b34427565b0dad1747bcb7c620d59cfc7b36
Martynas <47948087+MartSurv@users.noreply.github.com>
2020-04-23 10:42:57 +0300

    2020-04-23

--- Programa3.py
+++ Programa3.py
@@ -35,7 +35,7 @@ class Employee:
                         data.remove(emp)
                         with open(failas + '.json', 'w') as outfile:
                             json.dump(data, outfile, indent=4)
-                return emp
+                        return emp
             elif veiksmas == 'load':
                 for emp in data:
                     if emp['first'] == first and emp['last'] == last:
@@ -84,11 +84,15 @@ def user_input():
         if emp_type == 'Developer':
             new_dev = Developer(input('Name: '), input('Lastname: '), input('Pay: '), input('Programming language: '))
             Employee.add_emps(new_dev, 'developers')
+            print('Developeris', new_dev.first, new_dev.last, 'pridetas sekmingai.')
         elif emp_type == 'Manager':
             new_mgr = Manager(input('Name: '), input('Lastname: '), input('Pay: '))
             Employee.add_emps(new_mgr, 'managers')
+            print('Manageris', new_mgr.first, new_mgr.last, 'pridetas sekmingai.')
         else:
-            print('Klaida.')
+            print('-'*5)
+            print('Klaida ivedant darbuotojo tipa.')
+            print('-'*5)
 
     elif pasirinkimas == 2:
         emp_type = str(input('Iveskite darbuotojo tipa(Developer/Manager): '))
@@ -97,7 +101,9 @@ def user_input():
         elif emp_type == 'Manager':
             Employee.remove_load_emp(input('Name: '), input('Lastname: '), 'managers', 'del-load')
         else:
-            print('Klaida.')
+            print('-'*5)
+            print('Klaida ivedant darbuotojo tipa.')
+            print('-'*5)
 
     elif pasirinkimas == 3:
         print('-'*10, 'Developers', '-'*10)
@@ -119,32 +125,58 @@ def user_input():
     
     elif pasirinkimas == 4:
         emp_type = str(input('Iveskite darbuotojo tipa(Developer/Manager): '))
-        if emp_type == 'Developer':
-            x = Developer(**Developer.remove_load_emp(input('Name: '), input('Lastname: '), 'developers', 'del-load'))
-            x.apply_raise()
-            Employee.add_emps(x, 'developers')
+        try:
+            if emp_type == 'Developer':
+                x = Developer(**Developer.remove_load_emp(input('Name: '), input('Lastname: '), 'developers', 'del-load'))
+                x.apply_raise()
+                Employee.add_emps(x, 'developers')
+                print('Alga pakelta manageriui', x.first, x.last, 'sekmingai.')
 
-        elif emp_type == 'Manager':
-            x = Manager(**Manager.remove_load_emp(input('Name: '), input('Lastname: '), 'managers', 'del-load'))
-            x.apply_raise()
-            Employee.add_emps(x, 'managers')
-        else:
-            print('Klaida.')
+            elif emp_type == 'Manager':
+                x = Manager(**Manager.remove_load_emp(input('Name: '), input('Lastname: '), 'managers', 'del-load'))
+                x.apply_raise()
+                Employee.add_emps(x, 'managers')
+                print('Alga pakelta manageriui', x.first, x.last, 'sekmingai.')
+            else:
+                print('-'*5)
+                print('Klaida ivedant darbuotojo tipa.')
+                print('-'*5)
+        except TypeError:
+            print('-'*5)
+            print('Klaida ivedant varda ir pavarde. Pasitikrinkite esamus darbuotojus pasirinkus 3 meniu pasirinkima.')
+            print('-'*5)
     
     elif pasirinkimas == 5:
-        print('Iveskite Manager varda ir pavarde')
-        x = Manager(**Manager.remove_load_emp(input('Name: '), input('Lastname: '), 'managers', 'del-load'))
-        y = Developer(**Developer.remove_load_emp(input('Name: '), input('Lastname: '), 'developers', 'load'))
-        x.add_emp(y.__dict__)
-        Employee.add_emps(x, 'managers')
-        print('Developeris', y.first, y.last, 'priskirtas manageriui', x.first, x.last)
-    
+        try:
+            print('-'*3,'Iveskite Manager varda ir pavarde','-'*3)
+            x = Manager(**Manager.remove_load_emp(input('Name: '), input('Lastname: '), 'managers', 'del-load'))
+            print('-'*3,'Iveskite Developer varda ir pavarde','-'*3)
+            y = Developer(**Developer.remove_load_emp(input('Name: '), input('Lastname: '), 'developers', 'load'))
+            x.add_emp(y.__dict__)
+            Employee.add_emps(x, 'managers')
+            print('Developeris', y.first, y.last, 'priskirtas manageriui', x.first, x.last)
+        except TypeError:
+            print('-'*5)
+            print('Klaida ivedant varda ir pavarde. Pasitikrinkite esamus darbuotojus pasirinkus 3 meniu pasirinkima.')
+            print('-'*5)
+
     elif pasirinkimas == 6:
-        print('Iveskite Manager varda ir pavarde')
-        x = Manager(**Manager.remove_load_emp(input('Name: '), input('Lastname: '), 'managers', 'del-load'))
-        y = Developer(**Developer.remove_load_emp(input('Name: '), input('Lastname: '), 'developers', 'load'))
-        x.remove_emp(y.__dict__)
-        Employee.add_emps(x, 'managers')
-        print('Developeris', y.first, y.last, 'atskirtas nuo managerio', x.first, x.last)
+        try:
+            print('-'*3,'Iveskite Manager varda ir pavarde','-'*3)
+            x = Manager(**Manager.remove_load_emp(input('Name: '), input('Lastname: '), 'managers', 'del-load'))
+            print('-'*3,'Iveskite Developer varda ir pavarde','-'*3)
+            y = Developer(**Developer.remove_load_emp(input('Name: '), input('Lastname: '), 'developers', 'load'))
+            x.remove_emp(y.__dict__)
+            Employee.add_emps(x, 'managers')
+            print('Developeris', y.first, y.last, 'atskirtas nuo managerio', x.first, x.last)
+        except TypeError:
+            print('-'*5)
+            print('Klaida ivedant varda ir pavarde. Pasitikrinkite esamus darbuotojus pasirinkus 3 meniu pasirinkima.')
+            print('-'*5)
+
+    else:
+        print('-'*5)
+        print('Klaida ivedant pasirinkima is meniu.')
+        print('-'*5)
 meniu()
 user_input()
